f8f80d3dbf917805ed637d88529c8733b8c84b81
nivshemer <85192571+nivshemer@users.noreply.github.com>
2021-12-11 19:13:47 +0200

    Add files via upload

---
+++ AzureTask1.ps1
@@ -0,0 +1,253 @@
+﻿#Install the PowerShell Azure AD Module.
+#Install-Module AzureAD -force
+
+#Check that the AzureAD Powershell Module has been installed successfully 
+#Get-InstalledModule
+
+#module can be actually used
+#Get-Module -Listavailable
+
+# nivshemer@gmail.com AzureCloud  ******************************
+#Connect-AzureAD
+#get-azureaddomain
+#Connect-AzureAD -TenantId {YOUR_TENANT_ID}
+
+
+#install the Azure AD PowerShell module
+#install-module azuread
+#import-module azuread
+
+#Check that the module is ready to use
+#get-module azuread
+if (Get-Module -ListAvailable -Name AzureAD) {
+    Write-Log  "Module exists"
+} 
+else {
+    Write-Log  "Module does not exist, installing azuread"
+    install-module azuread
+    import-module azuread
+}
+#New session 
+#Connect-AzureAD
+
+#Retrieve existing groups from AD
+#get-azureadgroup
+
+#creates a new security group
+#New-AzureADGroup -Description "Marketing" -DisplayName "Marketing" -MailEnabled $false -SecurityEnabled $true -MailNickName "Marketing"
+
+#Filter groups
+#Get-AzureADGroup -Filter "DisplayName eq 'Varonis Assignment Group'"
+#Write-Log -Message "iteration end" 
+
+function Write-Log { 
+    param ( 
+        [string]$Message
+    ) 
+            
+    $logFile = "$Env:programdata\logs\log.log"
+    try 
+    { 
+        if((test-path $logFile))
+        {
+            if(Select-String -Path $logFile -Pattern "End of iteration")
+            {
+                $hour = (Get-Date -Format 'HH.mm.ss') 
+                Rename-Item -Path $logFile -NewName "log_$hour.log"
+            }
+        }
+        else
+        {
+            if(!(Test-Path -Path "$Env:programdata\logs" ))
+            {
+                New-Item -ItemType directory -Path "$Env:programdata\logs"  -ErrorAction SilentlyContinue -Verbose 
+            }
+        }
+        $DateTime = Get-Date -Format 'MM/dd/yy HH:mm:ss'        
+        [System.IO.File]::AppendAllText($logFile, "$DateTime - $Message" + [System.Environment]::NewLine)
+        
+        #Replplaced with [System.IO.File] because some issue that the log file was in use
+		Add-Content -Value "$DateTime - $Message" -Path $logFile -ErrorAction stop                            
+    } 
+    catch 
+    { 
+        Add-Content -Value "$DateTime $_.Exception.Message" -Path $logFile -ErrorAction stop    
+        Write-Log "ERROR: Creating Log file failed"
+        Write-Log $_.Exception.Message   
+    } 
+}
+
+
+
+function AddADGroupName()
+{
+    param(
+    [Parameter(Mandatory=$true)][string]$ADGroupName
+    )
+    try {
+        if(( (Get-AzureADGroup -Filter "DisplayName eq '$ADGroupName'") -eq $false ))
+        {
+            Write-Log  "Adding New ADGroupName to Azure Active Directory groups : $ADGroupName"
+            New-AzureADGroup -Description $ADGroupName -DisplayName $ADGroupName -MailEnabled $false -SecurityEnabled $true -MailNickName $ADGroupName
+        }
+        else
+        {
+             Write-Log  "AzureADGroup already exists: $ADGroupName"
+        }
+    
+    }
+    catch
+    {
+        Write-Error $_.Exception.Message
+        Write-Log  $_.Exception.Message 
+        Write-Log "End of iteration"
+        break
+    }
+    
+}
+
+
+function CheckADGroupNameExists()
+{
+    param(
+    [Parameter(Mandatory=$true)][string]$ADGroupName
+    )
+    try {
+        if(( (Get-AzureADGroup -Filter "DisplayName eq '$ADGroupName'") -eq $true ))
+        {
+            Return $true
+        }
+        else
+        {
+            Return $false
+        }
+    }
+    catch
+    {
+        Write-Error $_.Exception.Message
+        Write-Log "End of iteration"
+        break
+    }
+    
+}
+
+function AddUserToAZAD()
+{
+    param(
+    [Parameter(Mandatory=$true)][string]$DisplayName,
+    [Parameter(Mandatory=$true)][string]$UserPrincipalName,
+    [Parameter(Mandatory=$true)][string]$Password
+    )
+    try {
+        if((CheckAZUser -UserPrincipalName $UserPrincipalName) -eq $false)
+        {
+            Write-Log  "Adding New user to Azure Active Directory : $DisplayName, $UserPrincipalName, $Password"
+            New-AzureADUser -DisplayName $DisplayName -PasswordProfile  
+            -UserPrincipalName $UserPrincipalName -AccountEnabled $true -MailNickName $DisplayName        
+        }
+        else
+        {
+             Write-Log  "UserPrincipalName already exists: $UserPrincipalName"
+        }
+
+    
+    }
+    catch
+    {
+        Write-Error $_.Exception.Message
+        Write-Log "End of iteration"
+        break
+    }
+    
+}
+
+Function CheckAZUser()
+{
+  param(
+    [Parameter(Mandatory=$true)][string]$UserPrincipalName
+ )
+        #check if azure AD connection
+        try {
+                #check if user exists  
+                if($UserPrincipalName)
+                {
+                $UserPrincipalName = $UserPrincipalName.ToString()
+                $azureaduser = Get-AzureADUser -All $true | Where-Object {$_.Userprincipalname -eq "$UserPrincipalName"}
+                    #check if something found    
+                    if($azureaduser)
+                    {
+                        Write-Log  "User: $UserPrincipalName was found in $displayname AzureAD." 
+                        return $true
+                    }
+                    else
+                    {
+                        Write-Log  "User $UserPrincipalName was not found in $displayname Azure AD " 
+                        return $false
+                    }
+                }
+            }
+        catch
+            {
+                Write-Error $_.Exception.Message
+                Write-Log "End of iteration"
+                break
+            }
+
+}
+
+
+
+function AddUserToAZACGroup
+{
+  param(
+    [Parameter(Mandatory=$true)][string]$UserPrincipalName,
+    [Parameter(Mandatory=$true)][string]$GroupName
+ )
+
+        try {
+                $azureaduser = Get-AzureADUser -All $true | Where-Object {$_.Userprincipalname -eq "$UserPrincipalName"}
+                $GroupObj = Get-AzureADGroup -SearchString $GroupName
+                $UserObj = Get-AzureADUser -SearchString $azureaduser
+                Add-AzureADGroupMember -ObjectId $GroupObj.ObjectId -RefObjectId $UserObj.ObjectId
+                Write-Log  "User $UserPrincipalName was added successfully to Azure AD $GroupName at ((Get-Date).DateTime).ToString()" 
+            }
+        catch
+            {
+                Write-Log  "Could not Add user:$UserPrincipalName to GroupName:$GroupName at ((Get-Date).DateTime).ToString()" 
+                Write-Log "End of iteration"
+                break
+            }
+
+
+}
+
+function main 
+{
+
+    $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $PasswordProfile.Password = "P@ssw0rd"
+    $GroupName = "Varonis Assignment Group"
+    try{
+        AddADGroupName -ADGroupName $GroupName
+        if((CheckADGroupNameExists -ADGroupName $GroupName) -eq "$false")
+        {
+            Write-Error "ADGroupName - $GroupName doesn't exists"
+            break
+        }
+
+        for($i = 0; $i -le 20;$i++)
+        {
+            AddUserToAZAD -DisplayName "Test User $i" -UserPrincipalName "TestUser$i@Varonis.com" -Password $PasswordProfile.Password
+            AddUserToAZACGroup -UserPrincipalName "TestUser$i@Varonis.com" -GroupName $GroupName
+        }
+
+    }
+    catch
+    {
+        Write-Log "End of iteration"
+    }
+    
+
+}
+
+main
---
+++ AzureTask2.ps1
@@ -0,0 +1,73 @@
+﻿#Install-Module -Name Az
+#Connect-AzAccount
+
+$ResourceGroupName = 'NSM_group'
+$Location = 'EastUS'
+
+New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+$VaultName = 'VaronisAssignmentKv9'
+New-AzKeyVault -Name $VaultName -ResourceGroupName $ResourceGroupName -Location $Location
+
+
+
+Get-AzADUser
+
+$KVAccessPolicy = @{
+    UserPrincipalName         = 'nivshemer_gmail.com#EXT#@nivshemergmail.onmicrosoft.com'
+    VaultName                 = $VaultName
+    ResourceGroupName         = $ResourceGroupName
+    PermissionsToCertificates = @('Get','List','Delete','Create','Import','Update','Managecontacts','Getissuers','Listissuers','Setissuers','Deleteissuers','Manageissuers','Recover','Backup','Restore','Purge')
+    PermissionsToKeys         = @('Decrypt','Encrypt','UnwrapKey','WrapKey','Verify','Sign','Get','List','Update','Create','Import','Delete','Backup','Restore','Recover','Purge')
+    PermissionsToSecrets      = @('Get','List','Set','Delete','Backup','Restore','Recover','Purge')
+
+}
+
+Set-AzKeyVaultAccessPolicy @KVAccessPolicy
+
+
+$Cred = Get-Credential
+
+$SecretString = ConvertTo-SecureString -AsPlainText -Force -String ($Cred.UserName + "`v" + $Cred.GetNetworkCredential().Password)
+
+
+Set-AzKeyVaultSecret -VaultName $VaultName -Name 'TestCredential' -SecretValue $SecretString -ContentType 'PSCredential'
+#https://varonisassignmentkv9.vault.azure.net:443/secrets/TestCredential/871b1d08bb744f51b964b6cada10ff3b
+
+
+$VaultName = 'VaronisAssignmentKv9'
+
+$Secret = Get-AzKeyVaultSecret -VaultName $VaultName -Name 'TestCredential'
+$Secret.SecretValueText
+
+
+$Cred = New-Object System.Management.Automation.PSCredential (
+    ((Get-AzKeyVaultSecret -VaultName $VaultName -Name 'TestCredential').SecretValueText -Split "`v")[0],
+    (ConvertTo-SecureString ((Get-AzKeyVaultSecret -VaultName $VaultName -Name 'TestCredential').SecretValueText -Split "`v")[1] -AsPlainText -Force)
+)
+
+
+
+
+
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Get the current universal time in the default string format.
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+
+$Cred = New-Object System.Management.Automation.PSCredential (
+    ($env:TestCredential -Split "`v")[0],
+    (ConvertTo-SecureString ($env:TestCredential -Split "`v")[1] -AsPlainText -Force)
+)
+
+Write-Host "The Key Vault secret username is: $($Cred.UserName)"
+Write-Host "The Key Vault secret password is: $($Cred.GetNetworkCredential().Password)"
